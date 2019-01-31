4028a95c93d62b7588fbd5d7130c1a72aac05799
PironSteve <steve.piron@gmail.com>
2019-01-31 13:37:51 +0100

    build app

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -26999,7 +26999,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45555" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33593" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
